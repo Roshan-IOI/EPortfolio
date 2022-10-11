d1c1db2aa528dcbaf3b6da2ed9e55752ae2dae81
Roshan-IOI <rramaraj@lakeheadu.ca>
2022-10-11 02:28:37 -0400

    contact page design complete

--- public/javascripts/styles.js
+++ public/javascripts/styles.js
@@ -1,46 +1,19 @@
-var words = ['Hi i like HTML', 'I also like css', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'sed do eiusmod tempor incididunt'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 70;
-var wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    part = words[i].substr(0, offset);
-    if (skip_count == 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-    $('.word').text(part);
-  },speed);
-};
+//snippet of code from https://www.geeksforgeeks.org/how-to-play-video-on-mouse-hover-and-pause-on-mouseout-using-javascript/ 
+//for video play on mouse hover action
 
-$(document).ready(function () {
-  wordflick();
-});
+// Targeting video element 
+let clip = document.querySelector(".vid")
+  
+/* Applying mouseover event on video clip 
+and then we call play() function to play 
+the video when the mouse is over the video */
+clip.addEventListener("mouseover", function (e) {
+    clip.play();
+})
+
+/* Applying mouseout event on video clip 
+and then we call pause() function to stop 
+the video when the mouse is out the video */
+clip.addEventListener("mouseout", function (e) {
+    clip.pause();
+})
